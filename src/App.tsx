a5f7ce1a9d0cebabece43a45f41c4f438f63e3fe
ASMalakhovv <alsmalahovv@mail.ru>
2023-02-06 09:48:19 +0600

    add classnames

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./styles/theme/useTheme";
+import {classNames} from "./helpers/classnames/classnames";
 
 const App = () => {
     const {theme, toggleThemeClick} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {'aaaa': true}, [theme])}>
             <button onClick={toggleThemeClick}>Change theme</button>
             <Link to={'/'}>Главная </Link>
             <Link to={'/about'}>О сайте</Link>
---
+++ src/helpers/classnames/classnames.ts
@@ -0,0 +1,12 @@
+type Mods = Record<string, string | boolean>;
+
+export const classNames = (cls: string, mods: Mods, additional: string[]): string => {
+    return [
+        cls,
+        ...additional,
+        ...Object.entries(mods)
+            .filter(([classname, value]) => Boolean(value))
+            .map(([classname, value]) => classname)
+    ]
+        .join(' ');
+}
