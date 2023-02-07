3fdf40cd2acff4dd1df1ca594938242d738975f4
ASMalakhovv <alsmalahovv@mail.ru>
2023-02-07 23:39:01 +0600

    feature slice design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./styles/theme/useTheme";
-import {classNames} from "./helpers/classnames/classnames";
+import {useTheme} from "app/providers/ThemeProviders";
+import {classNames} from "../shared/lib/classnames/classnames";
+import { AboutPage } from 'pages/AboutPage';
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleThemeClick} = useTheme()
 
     return (
-        <div className={classNames('app', {'aaaa': true}, [theme])}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleThemeClick}>Change theme</button>
             <Link to={'/'}>Главная </Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
---
+++ src/app/providers/ThemeProviders/index.ts
@@ -0,0 +1,3 @@
+export {ThemeProvider} from './ui/ThemeProvider';
+export {useTheme} from './lib/useTheme';
+export {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from './lib/ThemeContext';
