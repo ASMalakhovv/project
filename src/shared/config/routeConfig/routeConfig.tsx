a30dd97953524d5d147c17acb66da9c0bc62a565
ASMalakhovv <alsmalahovv@mail.ru>
2023-02-18 12:31:21 +0300

    route config

---
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -0,0 +1,24 @@
+import {RouteProps} from "react-router-dom";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+
+enum AppRoutes {
+    MAIN = 'main',
+    ABOUT = 'about'
+}
+
+const routhPath: Record<AppRoutes, string> = {
+    [AppRoutes.MAIN]: '/',
+    [AppRoutes.ABOUT]: '/about'
+}
+
+export const routeConfig: Record<AppRoutes, RouteProps> = {
+    [AppRoutes.ABOUT]: {
+        path: routhPath.about,
+        element: <AboutPage />,
+    },
+    [AppRoutes.MAIN]: {
+        path: routhPath.main,
+        element: <MainPage />
+    }
+}
