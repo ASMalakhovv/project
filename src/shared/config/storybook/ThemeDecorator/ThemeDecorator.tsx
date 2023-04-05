e7c5ca41abd4f7dc39d4355e7f13b954c613dc9d
ASMalakhovv <alsmalahovv@mail.ru>
2023-04-06 00:15:38 +0600

    fix: eslint

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,10 +1,15 @@
 import { Story } from '@storybook/react';
 import { Theme, ThemeContext } from 'app/providers/ThemeProviders';
+import { useMemo } from 'react';
 
-export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
-    <ThemeContext.Provider value={{ theme }}>
-        <div className={`app ${theme}`}>
-            <StoryComponent />
-        </div>
-    </ThemeContext.Provider>
-);
+export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
+    const memoTheme = useMemo(() => ({ theme }), [theme]);
+
+    return (
+        <ThemeContext.Provider value={memoTheme}>
+            <div className={`app ${theme}`}>
+                <StoryComponent />
+            </div>
+        </ThemeContext.Provider>
+    );
+};
