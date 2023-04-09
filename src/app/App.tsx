0f1fc06f99f05546feb81b3ac0eedeb8cfd26c46
ASMalakhovv <alsmalahovv@mail.ru>
2023-04-09 21:59:25 +0600

    add Modal.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,24 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProviders';
 import { classNames } from 'shared/lib/classnames/classnames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 function App() {
     const { theme } = useTheme();
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur doloribus exercitationem facilis laboriosam nulla quod ratione reiciendis sunt veritatis voluptates!
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,77 @@
+import {
+    ReactNode, MouseEvent, useState, useRef, useEffect, useCallback,
+} from 'react';
+import { classNames } from 'shared/lib/classnames/classnames';
+
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string
+    children?: ReactNode
+    isOpen: boolean
+    onClose?: () => void
+}
+
+const ANIMATION_DELAY = 200;
+
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const refTimeoutId = useRef<ReturnType<typeof setTimeout>>();
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+        [cls.closed]: isClosing,
+    };
+
+    const handleOverlayClick = useCallback(() => {
+        setIsClosing(true);
+        if (onClose) {
+            setIsClosing(true);
+            refTimeoutId.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    }, [onClose]);
+
+    useEffect(() => {
+        const onKeyDown = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                handleOverlayClick();
+            }
+        };
+
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+
+        return () => {
+            clearTimeout(refTimeoutId.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [handleOverlayClick, isOpen]);
+
+    const handleContentClick = (e: MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    return (
+        <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={cls.overlay} onClick={handleOverlayClick}>
+                <div
+                    className={cls.content}
+                    onClick={handleContentClick}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
